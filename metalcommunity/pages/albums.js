452c1413f507175a999aba12f54d676947aa4058
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-07-28 18:17:41 -0500

    Pages

---
+++ metalcommunity/pages/albums.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Albums = () => {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Metal Community | Albums</title>
+            </Head>
+            <h1>Albums</h1>
+        </React.Fragment>
+    )
+}
+
+export default Albums;
---
+++ metalcommunity/pages/reviews.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Reviews = () => {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Metal Community | Reviews</title>
+            </Head>
+            <h1>Reviews</h1>
+        </React.Fragment>
+    )
+}
+
+export default Reviews;
